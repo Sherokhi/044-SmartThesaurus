5f148b3d4ded09b54fc16719c27f0fd300f00282
Loic <loicrossetbv@gmail.com>
2018-01-16 16:28:48 +0100

    Création d'un programme d'essai pour le K

---
+++ X-rossetlo-P_OO-loadthek/X-rossetlo-P_OO-loadthek/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace X_rossetlo_P_OO_loadthek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                //Créer un ensemble de fichiers situés dans le chemin spécifié
+
+                //Pour les fichiers qui contiennent la contrainte dans leur chemin
+                var files = from file in Directory.EnumerateFiles(@"H:\EssaiK", "*.txt", SearchOption.AllDirectories)
+                                //On lit chaque ligne du contenu du fichier
+                            from line in File.ReadLines(file)
+                                //On sélectionne que certains qui possède le contenu spécifié
+                            where file.Contains("asas")
+                            fdghfgfdd
+                            where line.Contains("asas")
+                            select new
+                            {
+                                File = file,
+                                Line = line
+                            };
+
+                //Pour chaque fichier dans l'ensemble de fichier, on affiche son chemin et son contenu
+                foreach (var f in files)
+                {
+                    Console.WriteLine("{0}", f.File);
+                }
+                //Nombre de fichiers trouvés
+                Console.WriteLine("{0} files found.", files.Count().ToString());
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                Console.WriteLine(UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                Console.WriteLine(PathEx.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
