a698011d1b9bae6e62c95707a3a89fd5fdd109b2
Jérémie Perret <perretje@etml.educanet2.ch>
2018-01-16 16:29:13 +0100

    Start Indexor for etml.ch
    Essaie de copie du texte

--- ThesaurusoIndexor/ThesaurusoIndexor/Form1.cs
+++ ThesaurusoIndexor/ThesaurusoIndexor/Form1.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.Net;
 
 namespace ThesaurusoIndexor
 {
@@ -23,16 +24,42 @@ namespace ThesaurusoIndexor
         {
             InitializeComponent();
             database = new DBConnect();
+            cbResearchLocation.SelectedIndex = 1;
         }
 
         private void btn_Research_Click(object sender, EventArgs e)
         {
+            switch (cbResearchLocation.SelectedIndex)
+            {
+                case 0: //Si la recherche doit chercher partout
 
+                    break;
+                case 1: //Si la recherche doit chercher sur etml.ch
+                    ETMLRes(txb_Research.Text);
+                    break;
+                case 2: //Si la recherche doit chercher sur educanet2.ch
+
+                    break;
+                case 3: //Si la recherche doit chercher sur K:\INF\ELEVE\Temp
+
+                    break;
+            }
         }
 
         private void txb_Research_TextChanged(object sender, EventArgs e)
         {
             btn_Research.Enabled = txb_Research.Text.Length > 0;
+        }
+
+        private void ETMLRes(string wordsToSearch)
+        {
+            WebClient client = new WebClient();
+            string[] contenu = new string[2000];
+
+            string[] subsStrings = wordsToSearch.Split(' ');
+
+
+            string codeHtml = client.DownloadString("https://www.etml.ch/");
         }
     }
 }
