360dd67dccf3734d52d632dd6130a3584bfe5d06
Loic <loicrossetbv@gmail.com>
2018-01-23 14:23:04 +0100

    Modification du console et du form
    Recherche sur le k effectuée grâce à la console et début d'implémentation du code sur le programme en windows form

---
+++ ThesaurusoIndexor/ThesaurusoIndexor/File.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThesaurusoIndexor
+{
+    public class File
+    {
+        private string name;
+        public string Name { get => name; set => name = value; }
+        private string path;
+        public string Path { get => path; set => path = value; }
+        private string extension;
+        public string Extension { get => extension; set => extension = value; }
+        private DateTime dateModif;
+        public DateTime DateModif { get => dateModif; set => dateModif = value; }
+        private int filePoids;
+        public int FilePoids { get => filePoids; set => filePoids = value; }
+        
+
+        public File(string strName, string strPath, string strExt, DateTime dateTimeModif, int intFilePoids)
+        {
+            name = strName;
+            path = strPath;
+            extension = strExt;
+            dateModif = dateTimeModif;
+            filePoids = intFilePoids;
+        }
+
+        public string ReturnFullName()
+        {
+            return this.name + this.extension;
+        }
+    }
+}
---
+++ ThesaurusoIndexor/ThesaurusoIndexor/ResearchK.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace algoResearch
+{
+    public class ResearchK
+    {
+        private string[] userSearch;
+
+        public ResearchK()
+        {
+
+        }
+
+        public void GetResearch(string strResearch)
+        {
+            userSearch = strResearch.Split(' ');
+        }
+
+        public void BeginTheReasearch()
+        {
+            foreach (string str in userSearch)
+            {
+                try
+                {
+                    //Pour les fichiers qui contiennent la contrainte dans leur chemin
+                    string[] files = Directory.GetFiles(@"K:\INF\eleves", "*" + str + "*", SearchOption.AllDirectories);
+                    foreach (string s in files)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    ////Nombre de fichiers trouvés
+                    Console.WriteLine("{0} files found.", files.Count().ToString());
+                }
+                catch (UnauthorizedAccessException UAEx)
+                {
+                    MessageBox.Show(UAEx.Message);
+                }
+                catch (PathTooLongException PathEx)
+                {
+                    MessageBox.Show(PathEx.Message);
+                }
+            }
+        }
+    }
+}
