139feee4c5a0680133ae6f7143c67526768aca50
Loic <loicrossetbv@gmail.com>
2018-01-23 15:43:20 +0100

    Fin de l'implémentation de la recherche dans le k

--- ThesaurusoIndexor/ThesaurusoIndexor/ResearchK.cs
+++ ThesaurusoIndexor/ThesaurusoIndexor/ResearchK.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
+using static ThesaurusoIndexor.Program;
+using System.Threading;
 
 namespace algoResearch
 {
     public class ResearchK
     {
         private string[] userSearch;
+        private static ResearchK actualResaerch;
 
         public ResearchK()
         {
-
+            
         }
 
         public void GetResearch(string strResearch)
@@ -22,20 +25,42 @@ namespace algoResearch
             userSearch = strResearch.Split(' ');
         }
 
+        public static ResearchK CreateResearch()
+        {
+            if(actualResaerch == null)
+            {
+                actualResaerch = new ResearchK();
+            }
+            return actualResaerch;
+        }
+
         public void BeginTheReasearch()
         {
+            string allData = "";
+            int counter = 0;
+            form1.rtbResult.Text = "";
+            form1.pbLoad.Value = 0;
             foreach (string str in userSearch)
             {
                 try
                 {
                     //Pour les fichiers qui contiennent la contrainte dans leur chemin
-                    string[] files = Directory.GetFiles(@"K:\INF\eleves", "*" + str + "*", SearchOption.AllDirectories);
+                    string[] files = Directory.GetFiles(@"K:\INF\Eleves\Temp\fichier", "*" + str + "*", SearchOption.AllDirectories);
+                    form1.pbLoad.Maximum = files.Count();
+                    form1.btn_Research.Enabled = false;
                     foreach (string s in files)
                     {
-                        Console.WriteLine(s);
+                        Thread.Sleep(20);
+                        counter++;
+                        allData += s + "\r\n";
+                        form1.rtbResult.Text = allData;
+                        form1.pbLoad.Value++;
+                        form1.Update();
+                        form1.lblResearchNumber.Text = counter.ToString();
                     }
+                    form1.btn_Research.Enabled = true;
                     ////Nombre de fichiers trouvés
-                    Console.WriteLine("{0} files found.", files.Count().ToString());
+                    form1.lblResearchNumber.Text = files.Count().ToString();
                 }
                 catch (UnauthorizedAccessException UAEx)
                 {
