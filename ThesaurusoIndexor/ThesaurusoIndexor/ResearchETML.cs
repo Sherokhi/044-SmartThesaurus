5a574326041355329673fc01fb5e25da81e7d4e9
Jérémie Perret <perretje@etml.educanet2.ch>
2018-01-30 16:23:44 +0100

    Finish ETML Research

--- ThesaurusoIndexor/ThesaurusoIndexor/ResearchETML.cs
+++ ThesaurusoIndexor/ThesaurusoIndexor/ResearchETML.cs
@@ -1,9 +1,16 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace algoResearch
 {
-    public class ResearchETML
+    class ResearchETML
     {
         public ResearchETML()
         {
@@ -19,17 +26,24 @@ namespace algoResearch
 
         public string getTextinHTML()
         {
-            WebRequest req = HttpWebRequest.Create("https://www.etml.ch/vie-de-lecole/menus-du-restaurant.html");
-            req.Method = "GET";
-
-            string source;
-            using (StreamReader reader = new StreamReader(req.GetResponse().GetResponseStream()))
-            {
-                source = reader.ReadToEnd();
-            }
-
-
-            return source;
+            var html = new WebClient().DownloadString("https://www.etml.ch/vie-de-lecole/menus-du-restaurant.html");
+            //string htmlTagPattern = "<.*?>";
+            var regexCss = new Regex("(\\<script(.+?)\\</script\\>)|(\\<style(.+?)\\</style\\>)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            html = regexCss.Replace(html, string.Empty);
+            //html = Regex.Replace(html, htmlTagPattern, string.Empty);
+            html = Regex.Replace(html, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+            html = html.Replace("&nbsp;", string.Empty);
+            Regex regEx = new Regex("<[^>]*>", RegexOptions.IgnoreCase);
+            html = regEx.Replace(html, "");
+            html = Regex.Replace(html, @"(\<!--\s*.*?((--\>)|$))", String.Empty);
+            html = Regex.Replace(html, @"\n", "");
+            html = Regex.Replace(html, @"&copy;", "");
+            //html = Regex.Replace(html, @"^((\<!--\s*.*?((--\>)|$))|\\n|<.*?>|&copy;)", String.Empty);
+            byte[] bytes = Encoding.Default.GetBytes(html);
+            html = Encoding.UTF8.GetString(bytes);
+            return html;
         }
+
+
     }
 }
