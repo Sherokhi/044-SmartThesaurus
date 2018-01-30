bb7fcba5039f333b5f9bcc9f759ff5f270f81d0d
Jérémie Perret <perretje@etml.educanet2.ch>
2018-01-30 16:22:49 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ ThesaurusoIndexor/ThesaurusoIndexor/ResearchETML.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Net;
+
+namespace algoResearch
+{
+    public class ResearchETML
+    {
+        public ResearchETML()
+        {
+
+        }
+
+        public string Start(string textToSearch)
+        {
+            string research = "";
+            string[] keyWords = textToSearch.Split(' ');
+            return research;
+        }
+
+        public string getTextinHTML()
+        {
+            WebRequest req = HttpWebRequest.Create("https://www.etml.ch/vie-de-lecole/menus-du-restaurant.html");
+            req.Method = "GET";
+
+            string source;
+            using (StreamReader reader = new StreamReader(req.GetResponse().GetResponseStream()))
+            {
+                source = reader.ReadToEnd();
+            }
+
+
+            return source;
+        }
+    }
+}
