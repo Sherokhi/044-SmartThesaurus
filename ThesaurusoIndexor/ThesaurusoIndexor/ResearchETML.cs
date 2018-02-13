462d28ec1e25336ea7c1eff301d97ccccc8e0d13
Jérémie Perret <perretje@etml.educanet2.ch>
2018-02-13 16:32:28 +0100

    Récupération des liens sur une page HTML
    Qui vont seulement sur le site de l'ETML

--- ThesaurusoIndexor/ThesaurusoIndexor/ResearchETML.cs
+++ ThesaurusoIndexor/ThesaurusoIndexor/ResearchETML.cs
@@ -1,64 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
 using System.Text.RegularExpressions;
 
 namespace algoResearch
 {
     class ResearchETML
     {
-        private string[] userSearch;
-        private static ResearchETML actualResearch;
-        private List<string> linkList = new List<string>();
+        private Dictionary<string, string> PagesResult = new Dictionary<string, string>();
+        string startURL = "https://www.etml.ch/vie-de-lecole/menus-du-restaurant.html";
+        string[] links;
         public ResearchETML()
         {
 
         }
 
-
-        public static ResearchETML CreateResearch()
+        public string Start(string textToSearch)
         {
-            if (actualResearch == null)
-            {
-                actualResearch = new ResearchETML();
-            }
-            return actualResearch;
+            string research = String.Empty;
+            string[] keyWords = textToSearch.Split(' ');
+            return research;
         }
-       
 
-        public string GetAllData()
+        public string getTextinHTML(string url)
         {
-            string html = "";
-            try
-            {
-                html= new WebClient().DownloadString("https://www.etml.ch/vie-de-lecole/menus-du-restaurant.html");
-            }
-            catch
-            {
-                throw new Exception("");
-            }
+            var html = new WebClient().DownloadString(url);
             //string htmlTagPattern = "<.*?>";
-            Match links = Regex.Match(html, "<a href=\".* \".*>");
             var regexCss = new Regex("(\\<script(.+?)\\</script\\>)|(\\<style(.+?)\\</style\\>)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
             html = regexCss.Replace(html, string.Empty);
             //html = Regex.Replace(html, htmlTagPattern, string.Empty);
-            html = Regex.Replace(html, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+            //html = Regex.Replace(html, @"^\s+$[\r\n]*", String.Empty, RegexOptions.Multiline);
             html = html.Replace("&nbsp;", string.Empty);
             Regex regEx = new Regex("<[^>]*>", RegexOptions.IgnoreCase);
-            html = regEx.Replace(html, "");
-            html = Regex.Replace(html, @"(\<!--\s*.*?((--\>)|$))", String.Empty);
-            html = Regex.Replace(html, @"\n", "");
-            html = Regex.Replace(html, @"&copy;", "");
-            //html = Regex.Replace(html, @"^((\<!--\s*.*?((--\>)|$))|\\n|<.*?>|&copy;)", String.Empty);
+            html = regEx.Replace(html, String.Empty);
+            //html = Regex.Replace(html, @"(\<!--\s*.*?((--\>)|$))", String.Empty);
+            html = Regex.Replace(html, @"\n", String.Empty);
+            html = Regex.Replace(html, @"\r", " ");
+            html = Regex.Replace(html, @"\t", " ");
+            //html = Regex.Replace(html, @"&copy;", String.Empty);
+            //\s*href\s *=\s*(\"([^"]*\")|'[^']*'|([^'">\s]+))
+            //<a([^>]?)>(.?)<\/a>
+            html = Regex.Replace(html, @"^((\<!--\s*.*?((--\>)|$))|\\n|<.*?>)", String.Empty);
             byte[] bytes = Encoding.Default.GetBytes(html);
             html = Encoding.UTF8.GetString(bytes);
+            html = Regex.Replace(html, @"[ ]{2,}", " ");
             return html;
         }
 
+        public string[] getLinks(string url)
+        {
+            string htmlCode = new WebClient().DownloadString(url);
+            var linkTags = Regex.Matches(htmlCode, "<a\\s*(.*)>(.*)</a>", RegexOptions.Multiline);
+            object[] links = new object[linkTags.Count];
+            linkTags.CopyTo(links, 0);
 
+            for (int i = 0; i < links.Length; i++)
+            {
+                links[i] = Regex.Replace(Regex.Match(links[i].ToString(), "\\s*href\\s*=\\s*(\"([^\"]*\")|'[^']*'|([^'\">\\s]+))").ToString(),"href=\"", String.Empty);
+                links[i] = Regex.Replace(links[i].ToString(), "\\s",String.Empty);
+            }
+            string[] finalLinks = new string[links.Length];
+            string[] allLinks = new string[links.Length];
+            for (int i = 0; i < links.Length; i++)
+            {
+                allLinks[i] = links[i].ToString();
+            }
+            int nbrEtmlLinks = 0;
+            foreach(string link in allLinks)
+            {
+                if(link[0] == '/' && link.Length > 2)
+                {
+                    finalLinks[nbrEtmlLinks] = link;
+                    nbrEtmlLinks++;
+                }
+            }
 
-
+            return finalLinks;
+        }
 
 
     }
