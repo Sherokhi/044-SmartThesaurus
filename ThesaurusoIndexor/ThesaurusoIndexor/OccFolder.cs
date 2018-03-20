40a92d6d8036cfb5217ed9f240109998f1cb4c85
rossetlo <rossetlo@etml.educanet2.ch>
2018-03-20 16:28:40 +0100

    Indexation de t_occurencefolder

---
+++ ThesaurusoIndexor/ThesaurusoIndexor/OccFolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThesaurusoIndexor
+{
+    public class OccFolder
+    {
+        public string folName;
+        public Dictionary<string, int> occWord;
+
+        //Constructeur de base
+        public OccFolder(string name)
+        {
+            folName = name;
+            occWord = new Dictionary<string, int>();
+        }
+
+        //Constructeur complet pour un fichier
+        public OccFolder(string name, Dictionary<string, int> dic)
+        {
+            folName = name;
+            occWord = dic;
+        }
+    }
+}
