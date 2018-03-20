f8b50dfb914d273b5e4b8bf3feba5c05ee988c1d
sherokhi <jeremie.perret.vuarrengel2000@gmail.com>
2018-03-20 16:29:04 +0100

    TrashCommit

---
+++ ThesaurusoIndexor/ThesaurusoIndexor/WebPage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace algoResearch
+{
+    public class WebPage
+    {
+        private string selfURL;
+        private int totalWordsOccurence;
+        private List<Word> words;
+
+        public WebPage(string pURL)
+        {
+            this.selfURL = pURL;
+            this.words = new List<Word>();
+            totalWordsOccurence = 0;
+        }
+
+        public void AddWordOccurence(string pWordToUp, int nbrOccurence)
+        {
+            bool found = false;
+            foreach(Word word in words)
+            {
+                if(word.Text == pWordToUp)
+                {
+                    word.NewOccurence(nbrOccurence);
+                    totalWordsOccurence += nbrOccurence;
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Word newWord = new Word(pWordToUp);
+                newWord.NewOccurence(nbrOccurence);
+                words.Add(newWord);
+            }
+        }
+    }
+}
---
+++ ThesaurusoIndexor/ThesaurusoIndexor/Word.cs
@@ -0,0 +1,21 @@
+﻿namespace algoResearch
+{
+    public class Word
+    {
+        private int occurences;
+        private string text;
+
+        public int Occurences { get => occurences;}
+        public string Text { get => text;}
+
+        public Word(string pText)
+        {
+            this.text = pText;
+        }
+
+        public void NewOccurence(int nbrOccurenceToAdd)
+        {
+            this.occurences += nbrOccurenceToAdd;
+        }
+    }
+}
